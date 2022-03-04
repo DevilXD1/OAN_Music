46d653722c2c8b19ef7ed2ae17e6769ce800a35a
Attitude Shaurya king <86145370+DevilXD1@users.noreply.github.com>
2022-03-04 17:47:44 +0530

    Update alive.py

--- handlers/alive.py
+++ handlers/alive.py
@@ -34,8 +34,8 @@ async def _human_time_duration(seconds):
 @Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/3d200a79842ed51e3c7df.jpg",
-        caption=f"""Hello miss/master I m Thor for music and fir ur group Security🛡️""",
+        photo=f"",
+        caption=f"""""",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
